922638003aa805f4a0d5d038d96d282ac014d23f
Blurple Calculator <awesomegamingofficial7@gmail.com>
2026-06-27 12:56:55 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,17 +11,9 @@ LOCAL_PATH := device/realme/RE54C1L1
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.mt6768
 
-# Core MediaTek Virtual Volume Utilities
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
     libdm \
     dtc \
     checkpoint_gc
-
-# Runtime Filesystem Support Engines
-PRODUCT_PACKAGES += \
-    e2fsck.recovery \
-    mke2fs.recovery \
-    tune2fs.recovery \
-    resize2fs.recovery
