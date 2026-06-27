a3bee1862d9d7df132e4bb0cd825374e607e381c
Blurple Calculator <awesomegamingofficial7@gmail.com>
2026-06-27 13:38:12 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,10 +10,3 @@ LOCAL_PATH := device/realme/RE54C1L1
 # Ensure allowlisted system configurations
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.mt6768
-
-# Core Client Packages (Fixed: Removed host tool 'dtc')
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    libdm \
-    checkpoint_gc
